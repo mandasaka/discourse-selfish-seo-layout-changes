275de3ed2ba1020f1f96cfb02780818897dd0f4b
mandasaka <i@unisj.com>
2022-08-01 12:09:44 +0800

    Update selfish_layout_changes.rb

--- app/lib/selfish_layout_changes.rb
+++ app/lib/selfish_layout_changes.rb
@@ -16,53 +16,6 @@ class SelfishSeoLayoutChanges
     end
   end
 
-  def self.modify_crawler_layout
-    if ENV["RAILS_ENV"] == "production"
-      tmp_file = "/shared/tmp/crawler_work.tmp.txt"
-    else
-      tmp_file = "#{Rails.root}/crawler_work.tmp.txt"
-    end
-    crawler_layout = "#{Rails.root}/app/views/layouts/crawler.html.erb"
-    if File.readlines(crawler_layout).grep(/www\.chirpset\.com/)&.empty?
-      powered_by_link = "\s\s\s\s\s\s" + '<p class="powered-by-link">Powered by <a href="https://chirpset.com/">chirpset.com</a>, best viewed with JavaScript enabled.</p>' + "\n"
-      if ENV["RAILS_ENV"] == "production"
-        tmp_file = "/shared/tmp/crawler_work.tmp.txt"
-      else
-        tmp_file = "#{Rails.root}/crawler_work.tmp.txt"
-      end
-
-      IO.foreach(crawler_layout) do |line|
-        if line.include? "powered-by-link"
-          IO.write(tmp_file, powered_by_link, mode: "a")
-        else
-          IO.write(tmp_file, line, mode: "a")
-        end
-      end
-      FileUtils.mv(tmp_file, crawler_layout)
-    end
-  end
-
-  def self.modify_application_layout
-    application_layout = "#{Rails.root}/app/views/layouts/application.html.erb"
-    if File.readlines(application_layout).grep(/www\.chirpset\.com/)&.empty?
-      powered_by_link = "\s\s\s\s\s\s" + '<p class="powered-by-link">Powered by <a href="https://chirpset.com/">chirpset.com</a>, best viewed with JavaScript enabled.</p>' + "\n"
-      if ENV["RAILS_ENV"] == "production"
-        tmp_file = "/shared/tmp/application_work.tmp.txt"
-      else
-        tmp_file = "#{Rails.root}/application_work.tmp.txt"
-      end
-
-      IO.foreach(application_layout) do |line|
-        if line.include? "powered_by_html"
-          IO.write(tmp_file, powered_by_link, mode: "a")
-        else
-          IO.write(tmp_file, line, mode: "a")
-        end
-      end
-      FileUtils.mv(tmp_file, application_layout)
-    end
-  end
-
   def self.modify_footer_layout
     footer_layout = "#{Rails.root}/app/views/layouts/_noscript_footer.html.erb"
     if File.readlines(footer_layout).grep(/www\.chirpset\.com/)&.empty?
