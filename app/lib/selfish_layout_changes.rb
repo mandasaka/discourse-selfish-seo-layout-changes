658f4bef681ea66205bbe78a9b9782568468aad0
mandasaka <i@unisj.com>
2022-08-01 11:50:48 +0800

    Update selfish_layout_changes.rb

--- app/lib/selfish_layout_changes.rb
+++ app/lib/selfish_layout_changes.rb
@@ -63,3 +63,25 @@ class SelfishSeoLayoutChanges
     end
   end
 end
+
+  def self.modify_footer_layout
+    footer_layout = "#{Rails.root}/app/views/layouts/_noscript_footer.html.erb"
+    if File.readlines(footer_layout).grep(/www\.chirpset\.com/)&.empty?
+      powered_by_link = "\s\s\s\s\s\s" + '<p class="powered-by-link">Powered by <a href="https://chirpset.com/">chirpset.com</a>, best viewed with JavaScript enabled.</p>' + "\n"
+      if ENV["RAILS_ENV"] == "production"
+        tmp_file = "/shared/tmp/footer_work.tmp.txt"
+      else
+        tmp_file = "#{Rails.root}/footer_work.tmp.txt"
+      end
+
+      IO.foreach(footer_layout) do |line|
+        if line.include? "powered_by_html"
+          IO.write(tmp_file, powered_by_link, mode: "a")
+        else
+          IO.write(tmp_file, line, mode: "a")
+        end
+      end
+      FileUtils.mv(tmp_file, footer_layout)
+    end
+  end
+end
